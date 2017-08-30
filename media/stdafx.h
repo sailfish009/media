5c05bc91a986d39a74f3070cdcce90fb8bf65be0
sailfish009 <sailfish009@gmail.com>
2017-08-30 13:40:39 +0900

    Update stdafx.h

--- media/stdafx.h
+++ media/stdafx.h
@@ -6,7 +6,8 @@
 #pragma once
 
 // Change these values to use different versions
-#define WINVER		0x0500
+//#define WINVER		0x0500
+#define WINVER _WIN32_WINNT_WIN7
 #define _WIN32_WINNT	0x0501
 #define _WIN32_IE	0x0501
 #define _RICHEDIT_VER	0x0500
